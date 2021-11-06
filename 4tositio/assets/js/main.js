b3bdbc77947162ce8434b4dc203fdf360bab911e
Rocaroto <72902886+Rocaroto@users.noreply.github.com>
2021-11-05 19:50:24 -0600

    Add files via upload

---
+++ 4tositio/assets/js/main.js
@@ -0,0 +1,12 @@
+function mover_pag(){
+    location.href="otroxd.html";
+}
+function mover_pag_2(){
+    location.href="index.html";
+}
+function mover_pag_3(){
+    location.href="https://boxicons.com/?query=";
+}
+function mover_pag_4(){
+    location.href="https://fonts.google.com/specimen/Fruktur#standard-styles";
+}
